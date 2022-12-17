fa9ce99b2a04152e999f6c5839395ff547ea71a9
Abraham Trinh <aqtrinh@asu.edu>
2022-12-17 04:51:57 -0700

    Debug Mobile

--- src/Components/Description.js
+++ src/Components/Description.js
@@ -3,6 +3,8 @@ export default function Description() {
     <div id="About" className="shadow bg-gray-100">
       <div className="flex flex-col py-10 md:py-20 text-3xl md:text-4xl outline-1">
         <p className="px-10 leading-tight max-w-5xl mx-auto text-black text-xl md:px-20 md:text-3xl tracking-tight sm:text-2xl">
+          <br/>
+          <br/>
           <strong>
           Hello, hello. 👋 My name is Abraham, I am a Software Engineer 🧑🏻‍💻 at <z className="text-mdtBlue">Medtronic</z> and an <z className="text-asuMaroon">Arizona State University</z>🔱 Alumni!
           </strong>
@@ -13,7 +15,9 @@ export default function Description() {
           returned to Medtronic to begin my journey as a Software Engineer.
           I am a person who is passionate about computer science and 
           I hope to utilize the skills that I learned in school and 
-          in the at work to write code that will benefit society! 
+          in the at work to write code that will benefit society!
+          <br/>
+          <br/> 
         </p>
       </div>
     </div>
