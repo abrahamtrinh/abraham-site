1ffd788066a31bcbcbceb2ee150500ede58e707c
Abraham Trinh <aqtrinh@asu.edu>
2022-12-21 03:50:57 -0700

    Change color of button (literally)

--- src/Components/Experience.js
+++ src/Components/Experience.js
@@ -153,7 +153,7 @@ export default function Experience() {
                       <a
                         href={experiences[currModal].image.href}
                         target="_blank"
-                        className="bg-myPurple-500 text-white active:bg-blue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                        className="bg-myPurple-500 text-white font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                         type="button"
                         onClick={() => setShowModal(false)}
                       >
