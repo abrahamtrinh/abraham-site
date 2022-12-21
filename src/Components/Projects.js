846392996bd62024605bdc9d37d47a278858d5ed
Abraham Trinh <aqtrinh@asu.edu>
2022-12-21 03:31:12 -0700

    Fixed modal line spacings

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -106,14 +106,6 @@ export default function Projects() {
                       <p className="text-sm lg:text-lg font-normal text-left">
                         <i>{projects[currModal].dates}</i>
                       </p>
-                      <button
-                        className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                        onClick={() => setShowModal(false)}
-                      >
-                        <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                          ×
-                        </span>
-                      </button>
                     </div>
                     {/*body*/}
                     <div className="relative p-6 flex-auto">
